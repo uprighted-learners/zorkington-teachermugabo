c029c9a2f2392976b60169765c3860e60f0e8b5a
Mugabo Uwilingiyimana <thierry.uwi@gmail.com>
2021-07-28 16:58:58 -0400

    formatting & comment changes - no code change

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
-const _ = require('underscore');
-const readline = require('readline');
+const _ = require('underscore')
+const readline = require('readline')
 
-const { createBrotliCompress } = require('zlib');
-const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+const { createBrotliCompress } = require('zlib')
+const readlineInterface = readline.createInterface(
+  process.stdin,
+  process.stdout
+)
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
-    readlineInterface.question(questionText, resolve);
-  });
+    readlineInterface.question(questionText, resolve)
+  })
 }
 
-// template for locations 
-// TODO change them to departments
+// template for locations
 class Location {
-  constructor(name, description = '', adjacent = [], inventory = [], locked = false) {
+  constructor(
+    name,
+    description = '',
+    adjacent = [],
+    inventory = [],
+    locked = false
+  ) {
     this.name = name
     this.description = description
     this.adjacent = adjacent
     this.inventory = inventory
     this.locked = locked
-
   }
 
   // returns description of location - convenience function
-  lookAround() { return this.getDescription() }
+  lookAround() {
+    return this.getDescription()
+  }
 
   // returns description of location
-  getDescription() { return this.description }
+  getDescription() {
+    return this.description
+  }
 
   canGo(location) {
     return this.adjacent.includes(location)
@@ -42,14 +53,16 @@ let mainEntrace = new Location(
   your shopping list on the hiring stand. You are currently at the main entrance.
   In front of you are the doors to Hannafords.  Next up is the cart room. 
   You'll need one. If you need help along the way, remember to just look around.`,
-  ['cart room'])
+  ['cart room']
+)
 let cartRoom = new Location(
   'cart room',
   `
   See the large carts on your right and small carts to your left.
   We're also hiring! Baskets are by the front produce`,
   ['front produce'],
-  ['big carts','small carts','shopping list'])
+  ['big carts', 'small carts', 'shopping list']
+)
 let frontProduce = new Location(
   'front produce',
   `
@@ -57,72 +70,97 @@ let frontProduce = new Location(
   and local strawberries. Yum! To your right are some green stuff you 
   can't quite make out. And to your right is a smorgasbord of fruit. 
   This was damn place is a smorgasbord!`,
-  ['cart room','back produce', 'left produce', 'right produce'],
-  ['strawberries','tomatoes'])
+  ['cart room', 'back produce', 'left produce', 'right produce'],
+  ['strawberries', 'tomatoes']
+)
 let backProduce = new Location(
   'back produce',
   `
   You're in the back produce -- looking at heaps of single avocadoes, and potatoes.
   There's also big juicy tomatoes, and peppers. There are also bags of onions and
   avocadoes as well.`,
-  ['front produce','right produce','left produce'],
-  ['avodaoes',' potatoes','big juicy tomatoes','peppers','onions','bag of onions','bag of avocadoes'])
+  ['front produce', 'right produce', 'left produce'],
+  [
+    'avodaoes',
+    ' potatoes',
+    'big juicy tomatoes',
+    'peppers',
+    'onions',
+    'bag of onions',
+    'bag of avocadoes',
+  ]
+)
 
-  // TODO add description
-  let leftProduce = new Location(
+// TODO add description
+let leftProduce = new Location(
   'left produce',
   `
   You're in the left produce -- tons of fruit!
   The usual suspects are here including some funky ones.
   Yes, hidden behind the passion fruit, are the mangoes and plums.
-  There are also a back of different berries! Take your pick!`,
-  ['checkout','back produce','front produce'],
-  ['passion fruit', 'mangoes', 'plums','blackberries', 'blueberries','strawberries'])
+  There are also a bunch of different berries! Take your pick!`,
+  ['checkout', 'back produce', 'front produce'],
+  [
+    'passion fruit',
+    'mangoes',
+    'plums',
+    'blackberries',
+    'blueberries',
+    'strawberries',
+  ]
+)
 
-  // TODO add description
-  let rightProduce = new Location(
+// TODO add description
+let rightProduce = new Location(
   'right produce',
   `You're in the right produce -- we have bell peppers and corn here.`,
-  ['back produce','front produce'],
-  ['corn','green bell peppers','yellow bell peppers','red bell peppers'])
-  
-  // TODO add description
-  let checkout = new Location(
+  ['back produce', 'front produce'],
+  ['corn', 'green bell peppers', 'yellow bell peppers', 'red bell peppers']
+)
+
+// TODO add description
+let checkout = new Location(
   'checkout',
-  `So you're ready to check out. Go everything on your list?`,
-  ['left produce']
-  ['receipt'])
+  `So you're ready to check out. Got everything on your list?`,
+  ['left produce']['receipt']
+)
 
 // locations & location instance lookupTable
 // TODO implementation of "forgiving" location names that doesn't
-//      require adding all these possibilities into the Location 
+//      require adding all these possibilities into the Location
 //      class instance's adjacent array.
 //      ISSUE: currently, adding locations here isn't enough, I need
 //      to update locations in each Location class instance as well.
 //      seems like a bad/poor pattern!
 const locationLookUp = {
-  "main entrance" : mainEntrace,
-  "cart room" : cartRoom,
+  'main entrance': mainEntrace,
+  'cart room': cartRoom,
   //"cartroom" : cartRoom,
   //"carts": cartRoom,
-  "front produce" : frontProduce,
+  'front produce': frontProduce,
   //"front" : frontProduce,
-  "back produce" : backProduce,
+  'back produce': backProduce,
   //"back" : backProduce,
-  "left produce" : leftProduce,
+  'left produce': leftProduce,
   //"left" : leftProduce,
   //"left side" : leftProduce,
-  "right produce" : rightProduce,
+  'right produce': rightProduce,
   //"right" : rightProduce,
   //"right side" : rightProduce,
-  "checkout" : checkout
+  checkout: checkout,
 }
 
 // TODO add items
 // items - these will simply be shopping list, cart (could extend to cash register)
-// TODO - what items will be untakable? 
+// TODO - what items will be untakable?
 class Item {
-  constructor(name, description = '', contents = [], available = false, takeable = false) {
+  constructor(
+    name,
+    description = '',
+    contents = [],
+    available = false,
+    takeable = false
+  ) {
     this.name = name
     this.contents = contents
     this.available = available
@@ -130,7 +168,7 @@ class Item {
   }
 }
 
-// Method to create the shopper's list 
+// Method to create the shopper's list
 const SHOPPING_LIST_LENGTH = 5
 
 // create produce inventory
@@ -141,58 +179,44 @@ let createProduceInventory = () => {
     .concat(leftProduce.inventory)
     .concat(rightProduce.inventory)
 }
+
 const produceInventory = createProduceInventory()
 
 // Create shopping list
 let createShoppingList = () => {
-  // TODO #0 randomly generate a list of 5 items to buy
   return _.take(_.shuffle(produceInventory), SHOPPING_LIST_LENGTH)
 }
 
-let shoppingList = createShoppingList();
+let shoppingList = createShoppingList()
 // console.log(shoppingList) -> five random items to shop for.
 
-// TODO #1 -- 
+// TODO next
 //  create items (use locations descriptions & pictures)
 //  decide on properties of the items -- takeable and such
 
-// Possible future extensions
-// 
-/*
-move forward
-move back
-move left
-move right
-take [object]
-*/
+// Possible future extensions:
+// - ability to move forward, back, left, right
 
 let player = {
   // set player's current location
-  name: "Bob",
-  currentLocation: "main entrance",
+  name: 'Bob',
+  currentLocation: 'main entrance',
   // allowed actions
-  actions: [ "go", "go to","take","return","pay","leave","look"],
+  actions: ['go', 'go to', 'take', 'return', 'pay', 'leave', 'look'],
   cart: [],
-  hasReceipt: false
+  hasReceipt: false,
 }
 
-// print out current place's description
+// Set up game -- describe current location for the user
 console.log(locationLookUp[player.currentLocation].getDescription())
 
-
-start();
+// let the games begin! -- recursively called to loop. Exit with 'leave'
+start()
 
 async function start() {
-
   // user prompt
-  const prompt = 
-
-`
-What to do next? >_`
-
-  // prompt user
-  let answer = await ask(prompt);
-  // console.log(answer, 'line 174');
+  const prompt = '\n\nWhat to do next? >_'
+  let answer = await ask(prompt)
 
   // quick & dirty sanitizing
   let inputArray = answer.trim().toLowerCase().split(' ')
@@ -204,8 +228,8 @@ What to do next? >_`
   // + ability to process "go to"
   // TODO replace this logic with user input lookupTable
   action = inputArray[0] // first word
-  if ( action === "go" && inputArray[1] === "to" ) {
-    action = "go to"
+  if (action === 'go' && inputArray[1] === 'to') {
+    action = 'go to'
     target = inputArray.slice(2).join(' ') // the rest
   }
   // if action is not "go to", expect single word for command
@@ -215,88 +239,91 @@ What to do next? >_`
 
   // execute user's wishes
   // if action isn't known, let user know
-  if ( !player.actions.includes(action) ) {
+  if (!player.actions.includes(action)) {
     console.log(
-    `Dear Shopper. You're limited to a few commands. 
+      `Dear Shopper. You're limited to a few commands. 
       You can go to [place], take [item], drop [item], etc...
-      Plz, try again.`)
-  } 
+      Plz, try again.`
+    )
+  }
   // if action is known, let user know as well
   else {
     console.log(`Ah, so you want want to ${action} ${target}.`)
 
-    // if look - assume "look around" - get current place's location & look out! 
-    if ( action === 'look' ) {
+    // if look - assume "look around" - get current place's location & look out!
+    if (action === 'look') {
       // TODO explore using text wrapping - currently using `` and formatting as desired.
       // https://stackoverflow.com/questions/14484787/wrap-text-in-javascript
       console.log(locationLookUp[player.currentLocation].lookAround())
     }
     // ===     logic to move b/t locations ===== //
-    // if "go", expect/check target is location, 
-    else if ( action === "go to" || action === "go" ) {
+    // if "go", expect/check target is location,
+    else if (action === 'go to' || action === 'go') {
       // check/handle if valid location
-      if(Object.keys(locationLookUp).includes(target)) {
+      if (Object.keys(locationLookUp).includes(target)) {
         // check/handle if allowed transition
         // TODO story -- when leaving main entrance to cart room, main entrance locks!
-        //               can't leave w/o the full shopping leave.  
-        // TODO story -- when shopper returns to main entrance w/ everything, unlock it! 
+        //               can't leave w/o the full shopping leave.
+        // TODO story -- when shopper returns to main entrance w/ everything, unlock it!
         if (locationLookUp[player.currentLocation].canGo(target)) {
-          console.log(`Good guess! You left ${player.currentLocation} and are now in ${target}`)
+          console.log(
+            `Good guess! You left ${player.currentLocation} and are now in ${target}`
+          )
           player.currentLocation = target
         } else {
           // TODO implement can't go from here to there -- if we got a valid location
-          console.log(`Can't go from ${player.currentLocation} to ${target}. Clues, shopper, clues.`)
+          console.log(
+            `Can't go from ${player.currentLocation} to ${target}. Clues, shopper, clues.`
+          )
         }
       } else {
         // wrong location -- help user with location
         console.log(`Hmmm, don't know ${target}. Look around!`)
-      }   
+      }
     }
     // examine shopping list & cart
-    else if ( action == "examine" ) {
+    else if (action == 'examine') {
       // non-produce items  -- shopping list, cart, inventory
-      if ( target === "shopping list") {
+      if (target === 'shopping list') {
         console.log(`Awesome! Here's what you need to get: `, shoppingList)
-      } else if ( target == "cart" ) {
+      } else if (target == 'cart') {
         console.log(`Here's what you got in your cart: `, player.cart)
-      } 
+      }
     }
     // TODO #3 implement "take" actions, expect take [item]
-    else if ( action == "take" ) {
+    else if (action == 'take') {
       // non-produce items  -- shopping list, cart, inventory
-      if ( target === "shopping list") {
+      if (target === 'shopping list') {
         console.log(`Awesome! Here's what you need to get: `, shoppingList)
-      } else if ( target == "cart" ) {
+      } else if (target == 'cart') {
         console.log(`You got yourself a cart -- now let's get shopping!`)
-      } 
+      }
       // TODO #3b add take inventory (as item) => list what user has
-      else if ( target == "inventory" ) {
+      else if (target == 'inventory') {
         console.log(`Here's what you got so far:`, player.cart)
       } else {
-
       }
 
       // taking a produce item
-
     }
     // TODO #4 if "drop" expect target = [item], check
-    else if ( action === 'drop') {
-
+    else if (action === 'drop') {
     }
     // TODO #5 if "pay", say thanks - you're all set
-    else if ( action === 'pay') console.log(
-      `
+    else if (action === 'pay')
+      console.log(
+        `
       Thanks for the cashless exchange! You're all set. 
-      You can leave now. You remember where you came from, don't you?`)
+      You can leave now. You remember where you came from, don't you?`
+      )
     // if "leave" say bye
-    else if (action === 'leave') { 
-      // TODO - Have they opened the main entrance? 
+    else if (action === 'leave') {
+      // TODO - Have they opened the main entrance?
       //        If not, give them a hard time and release them.
 
-      console.log("bye")
+      console.log('bye')
       process.exit(0)
     }
-
   }
 
   start()
